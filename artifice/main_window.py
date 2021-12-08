c619c7d0b2530a9b10b0e7a0329c5ff16b2935e1
[CorwinAnsley] <[coreyansley@gmail.com]>
2021-12-08 14:14:57 +0000

    added start and view rampart buttons to rampart tab

--- artifice/main_window.py
+++ artifice/main_window.py
@@ -4,6 +4,12 @@ import os.path
 import json
 import selection_window
 import parse_columns_window
+import csv
+from webbrowser import open_new_tab
+
+
+RAMPART_PORT_1 = 1100
+RAMPART_PORT_2 = 1200
 
 #defines the layout of the window
 def setup_layout(theme='Dark'):
@@ -52,7 +58,11 @@ def setup_layout(theme='Dark'):
     ]
 
     rampart_tab = [
-    [sg.Text('Rampart',size=(12,1)),]
+    [sg.Text('Rampart',size=(12,1)),],
+    [
+    sg.Button(button_text='Start Rampart',key='-START RAMPART-'),
+    sg.Button(button_text='View Rampart',key='-VIEW RAMPART-'),
+    ],
     ]
 
     tabs_column = [
@@ -203,6 +213,43 @@ def update_run_list(window, run_info, run_to_select= ''):
     run_info = load_run(window, run_to_select)
     return run_info
 
+def prepare_analysis(run_info):
+    json_dict = {}
+
+    if not len(run_info['name']) > 0:
+        raise Exception('Invalid Name')
+
+    if os.path.isfile(run_info['samples']) == False:
+        raise Exception('Invalid samples file')
+
+    if os.path.isdir(run_info['MinKnow']) == False:
+        raise Exception('Invalid MinKnow')
+
+    try:
+        os.makedirs('resources/template_config')
+    except:
+        pass
+    try:
+        os.mkdir('rampart')
+    except:
+        pass
+
+    json_dict['basecalledPath'] = run_info['MinKnow']
+    json_dict['title'] = str(run_info['name'])
+    with open('resources/template_config/run_configuration.json', 'w') as jsonfile:
+        jsonfile.write(json.dumps(json_dict))
+
+    barcodes_list = parse_columns_window.csv_to_list(run_info['barcodes'])
+    with open('resources/template_config/barcodes.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for row in barcodes_list:
+            csvwriter.writerow(row)
+
+def run_analysis(firstPort = 1100, secondPort = 1200):
+    rampart_dir = os.getcwd()+ '/rampart'
+    start_rampart.start_rampart(rampart_dir, firstPort = firstPort, secondPort = secondPort)
+
+
 def create_main_window(theme = 'Dark', font = ('FreeSans', 18), window = None):
     layout = setup_layout()
     new_window = sg.Window('Artifice', layout, font=font, resizable=True)
@@ -266,12 +313,33 @@ def run_main_window(window, font = ('FreeSans', 18)):
                 sg.popup_error(err)
 
         elif event == '-DELETE RUN-':
-            user_confirm = sg.popup_ok_cancel('Are you sure you want to delete this run?',font=font)
-            if user_confirm != 'OK':
-                continue
-            delete_run(run_info['name'], window)
-            run_info = {}
-            run_info = update_run_list(window, run_info)
+            try:
+                user_confirm = sg.popup_ok_cancel('Are you sure you want to delete this run?',font=font)
+                if user_confirm != 'OK':
+                    continue
+                delete_run(run_info['name'], window)
+                run_info = {}
+                run_info = update_run_list(window, run_info)
+            except Exception as err:
+                sg.popup_error(err)
+
+        elif event == '-SAVE RUN-':
+            try:
+                prepare_analysis(run_info)
+                run_analysis(firstPort=RAMPART_PORT_1, secondPort=RAMPART_PORT_2)
+            except Exception as err:
+                sg.popup_error(err)
+
+        elif event == '-START RAMPART-':
+            try:
+                prepare_analysis(run_info)
+                run_analysis(firstPort=RAMPART_PORT_1, secondPort=RAMPART_PORT_2)
+            except Exception as err:
+                sg.popup_error(err)
+
+        elif event == '-VIEW RAMPART-':
+            address = 'http://localhost:'+str(RAMPART_PORT_1)
+            open_new_tab(address)
 
 
     window.close()
