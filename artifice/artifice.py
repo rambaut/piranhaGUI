64e4f54d46368804556bd99f80e1c801c99b83cf
[CorwinAnsley] <[coreyansley@gmail.com]>
2022-01-27 14:07:14 +0000

    added imports for pyinstaller

--- artifice/artifice.py
+++ artifice/artifice.py
@@ -4,6 +4,10 @@ import json
 
 import consts
 import main_window
+import selection_window
+import parse_columns_window
+import start_rampart
+import view_barcodes_window
 
 def check_runs_dir(runs_dir):
     filepath = runs_dir + '/archived_runs.json'
