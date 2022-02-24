904daa28807bbf4160806878404ddec7177e4272
CorwinAnsley <coreyansley@gmail.com>
2022-02-24 18:06:11 +0000

    refactoring code for rampart tab

--- artifice/main_window.py
+++ artifice/main_window.py
@@ -3,10 +3,8 @@ from os import listdir, mkdir, remove, getcwd, rename
 import os.path
 import requests
 import json
-from webbrowser import open_new_tab
 from shutil import rmtree, move, copytree
 from datetime import datetime
-from time import sleep
 import traceback
 
 import consts
@@ -16,6 +14,7 @@ import start_rampart
 import view_barcodes_window
 from update_log import log_event, update_log
 from infotab import infotab_event
+from rampart_tab import rampart_tab_event, check_rampart_running
 from manage_runs import save_run, delete_run, clear_selected_run, update_run_list, get_runs, save_changes
 
 def make_theme():
@@ -107,23 +106,20 @@ def setup_layout(theme='Dark', font = None):
         rampart_button_text = 'Start RAMPART'
         rampart_status = 'RAMPART is not running'
 
-
-
-
     rampart_tab = [
-    [sg.Button(button_text='View Barcodes',key='-VIEW BARCODES-'),],
+    [sg.Button(button_text='View Barcodes',key='-RAMPART TAB-VIEW BARCODES-'),],
     [sg.Text(rampart_status, key='-RAMPART STATUS-'),],
     [
-    sg.Button(button_text=rampart_button_text,key='-START/STOP RAMPART-'),
-    sg.Button(button_text='View RAMPART', visible=rampart_running,key='-VIEW RAMPART-'),
+    sg.Button(button_text=rampart_button_text,key='-RAMPART TAB-START/STOP RAMPART-'),
+    sg.Button(button_text='View RAMPART', visible=rampart_running,key='-RAMPART TAB-VIEW RAMPART-'),
     ],
     [
     sg.VPush()
     ],
     [
-    sg.Button(button_text='< Info', key='-TO INFO-'),
+    sg.Button(button_text='< Info', key='-RAMPART TAB-TO INFO-'),
     sg.Push(),
-    sg.Button(button_text='PIRANHA >', key='-TO PIRANHA-'),
+    sg.Button(button_text='PIRANHA >', key='-RAMPART TAB-TO PIRANHA-'),
     ],
     ]
 
@@ -172,18 +168,6 @@ def setup_layout(theme='Dark', font = None):
 
     return layout, rampart_running
 
-
-def check_rampart_running():
-    try:
-        r = requests.get(f'http://localhost:{consts.RAMPART_PORT_1}')
-        if r.status_code == 200:
-            update_log(f'detected RAMPART running on port: {consts.RAMPART_PORT_1}')
-            return True
-        else:
-            return False
-    except:
-        return False
-
 def create_run(font=None):
     update_log(f'creating new run')
     window = selection_window.create_select_window(font=font)
@@ -216,51 +200,6 @@ def create_run(font=None):
     update_log(f'created run: "{title}" successfully')
 
     return title
-
-
-def launch_rampart(run_info, client, firstPort = 1100, secondPort = 1200, runs_dir = consts.RUNS_DIR, font = None, container = None):
-    if 'title' not in run_info or not len(run_info['title']) > 0:
-        raise Exception('Invalid Name/No Run Selected')
-    title = run_info['title']
-    update_log(f'launching RAMPART on run: "{title}"')
-    if 'samples' not in run_info or os.path.isfile(run_info['samples']) == False:
-        raise Exception('Invalid samples file')
-    if 'basecalledPath' not in run_info or os.path.isdir(run_info['basecalledPath']) == False:
-        raise Exception('Invalid MinKnow')
-
-    basecalled_path = run_info['basecalledPath']
-
-    config_path = runs_dir+'/'+title+'/run_configuration.json'
-
-    try:
-        with open(config_path,'r') as file:
-            run_configuration = json.loads(file.read())
-    except:
-        run_configuration = {}
-
-    run_configuration['title'], run_configuration['basecalledPath'] = run_info['title'], run_info['basecalledPath']
-
-    with open(config_path, 'w') as file:
-        config_json = json.dump(run_configuration, file)
-        #file.write(config_json)
-
-    view_barcodes_window.check_barcodes(run_info,font=font)
-
-    run_path = runs_dir+'/'+run_info['title']
-    container = start_rampart.start_rampart(run_path, basecalled_path, client, consts.DOCKER_IMAGE, firstPort = firstPort, secondPort = secondPort, container=container)
-
-    iter = 0
-    while True:
-        sleep(0.1)
-        iter += 1
-        if iter > 100:
-            raise Exception('Something went wrong launching RAMPART')
-        try:
-            rampart_running = check_rampart_running()
-            if rampart_running:
-                return container
-        except:
-            pass
 
 def create_main_window(theme = 'Artifice', font = None, window = None):
     update_log('creating main window')
@@ -328,7 +267,18 @@ def run_main_window(window, font = None, rampart_running = False):
             window['-PIRANHA TAB-'].select()
 
         elif event.startswith('-INFOTAB-'):
-            run_info, selected_run_title = infotab_event(event, run_info, selected_run_title, hide_archived, font, values, window)
+            try:
+                run_info, selected_run_title = infotab_event(event, run_info, selected_run_title, hide_archived, font, values, window)
+            except Exception as err:
+                update_log(traceback.format_exc())
+                sg.popup_error(err)
+
+        elif event.startswith('-RAMPART TAB-'):
+            try:
+                run_info, docker_client, rampart_container, rampart_running = rampart_tab_event(event, run_info, docker_client, rampart_container, rampart_running, font)
+            except Exception as err:
+                update_log(traceback.format_exc())
+                sg.popup_error(err)
 
         elif event == '-RUN LIST-':
             try:
@@ -393,49 +343,6 @@ def run_main_window(window, font = None, rampart_running = False):
                 runlist_visible = True
 
 
-        elif event == '-VIEW BARCODES-':
-            try:
-                view_barcodes_window.check_barcodes(run_info, font=font)
-
-                barcodes = consts.RUNS_DIR+'/'+run_info['title']+'/barcodes.csv'
-                barcodes_window, column_headers = view_barcodes_window.create_barcodes_window(barcodes,font=font)
-                view_barcodes_window.run_barcodes_window(barcodes_window,barcodes,column_headers)
-            except Exception as err:
-                update_log(traceback.format_exc())
-                sg.popup_error(err)
-
-        elif event == '-START/STOP RAMPART-':
-            try:
-                if rampart_running:
-                    #try:
-                        #print(rampart_container.top())
-                    #except:
-                    #    pass
-                    rampart_running = False
-                    start_rampart.stop_docker(client=docker_client, container=rampart_container)
-                    window['-VIEW RAMPART-'].update(visible=False)
-                    window['-START/STOP RAMPART-'].update(text='Start RAMPART')
-                    window['-RAMPART STATUS-'].update('RAMPART is not running')
-                else:
-                    rampart_container = launch_rampart(run_info, docker_client, firstPort=consts.RAMPART_PORT_1, secondPort=consts.RAMPART_PORT_2, font=font, container=rampart_container)
-                    rampart_running = True
-                    window['-VIEW RAMPART-'].update(visible=True)
-                    window['-START/STOP RAMPART-'].update(text='Stop RAMPART')
-                    window['-RAMPART STATUS-'].update('RAMPART is running')
-
-                    #print(rampart_container.logs())
-            except Exception as err:
-                update_log(traceback.format_exc())
-                sg.popup_error(err)
-
-        elif event == '-VIEW RAMPART-':
-            address = 'http://localhost:'+str(consts.RAMPART_PORT_1)
-            update_log(f'opening address: "{address}" in browser to view RAMPART')
-            try:
-                open_new_tab(address)
-            except Exception as err:
-                update_log(traceback.format_exc())
-                sg.popup_error(err)
 
     window.close()
 
