72d251c3caac781b4f031a1d655f82e886407c2b
CorwinAnsley <coreyansley@gmail.com>
2022-03-23 11:40:00 +0000

    slight optimisation to piranha image

--- artifice/docker_piranha/build.sh
+++ artifice/docker_piranha/build.sh
@@ -4,15 +4,15 @@
 # Build the compile stage:
 docker build --target compile-image \
        --build-arg BUILDKIT_INLINE_CACHE=1 \
-       --cache-from=artifice_piranha:compile-stage \
-       --tag artifice_piranha:compile-stage .
+       --cache-from=coreyansley/artifice_piranha:compile-stage \
+       --tag coreyansley/artifice_piranha:compile-stage .
 
 # Build the runtime stage, using cached compile stage:
 docker build --target runtime-image \
        --build-arg BUILDKIT_INLINE_CACHE=1 \
-       --cache-from=artifice_piranha:compile-stage \
-       --cache-from=artifice_piranha:latest \
-       --tag artifice_piranha:latest .
+       --cache-from=coreyansley/artifice_piranha:compile-stage \
+       --cache-from=coreyansley/artifice_piranha:latest \
+       --tag coreyansley/artifice_piranha:latest .
 
 # Push the new versions:
 #docker push itamarst/helloworld:compile-stage
