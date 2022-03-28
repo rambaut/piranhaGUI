3f53474bdd983921b108dd855b9c822c082001e6
CorwinAnsley <coreyansley@gmail.com>
2022-03-28 16:11:50 +0100

    rampart image shrunk further

--- artifice/docker_rampart/build.sh
+++ artifice/docker_rampart/build.sh
@@ -4,15 +4,15 @@
 # Build the compile stage:
 docker build --target compile-image \
        --build-arg BUILDKIT_INLINE_CACHE=1 \
-       --cache-from=artifice_polio_rampart:compile-stage \
-       --tag artifice_polio_rampart:compile-stage .
+       --cache-from=coreyansley/artifice_polio_rampart:compile-stage \
+       --tag coreyansley/artifice_polio_rampart:compile-stage .
 
 # Build the runtime stage, using cached compile stage:
 docker build --target runtime-image \
        --build-arg BUILDKIT_INLINE_CACHE=1 \
-       --cache-from=artifice_polio_rampart:compile-stage \
-       --cache-from=artifice_polio_rampart:latest \
-       --tag artifice_polio_rampart:latest .
+       --cache-from=coreyansley/artifice_polio_rampart:compile-stage \
+       --cache-from=coreyansley/artifice_polio_rampart:latest \
+       --tag coreyansley/artifice_polio_rampart:latest .
 
 # Push the new versions:
 #docker push itamarst/helloworld:compile-stage
