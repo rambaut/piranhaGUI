57f8a3f1720ade1541482f3f878f5d054956cbb9
CorwinAnsley <2266643A@student.gla.ac.uk>
2022-08-02 13:01:28 +0100

    updated config/data dir

--- artifice/artifice_core/consts.py
+++ artifice/artifice_core/consts.py
@@ -17,8 +17,9 @@ def get_datadir():
     else: #linux
         os_path = getenv("XDG_DATA_HOME", "~/.local/share")
 
-    #path = Path(os_path) / "ARTIFICE"
-    path = Path(os_path) / "piranhaGUI"
+    path = Path(os_path) / "ARTIFICE"
+    #path = Path(os_path) / "piranhaGUI"
+
     path = path.expanduser()
 
     if not os.path.isdir(path): #creates data directory if it doesn't exist
