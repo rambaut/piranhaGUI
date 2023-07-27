b3f07fae258c67cea56d382a7fb5876d2b34c5fb
Andrew Rambaut <rambaut@gmail.com>
2023-07-27 13:21:15 +0300

    Clean up of the samples table window

--- artifice/artifice_core/parse_columns_window.py
+++ artifice/artifice_core/parse_columns_window.py
@@ -42,8 +42,10 @@ def samples_to_list(filepath, has_headers = True, trim = True):
 
     return samples_list, column_headers
 
-def setup_panel(samples, samples_column = 0, barcodes_column = 1, has_headers = True):
+def setup_panel(samples, barcodes_column = 0, samples_column = 1, has_headers = True):
     sg.theme('PANEL')
+
+    theme=consts.THEMES[sg.theme()]
 
     samples_list, column_headers = samples_to_list(samples, has_headers=has_headers)
 
@@ -57,26 +59,35 @@ def setup_panel(samples, samples_column = 0, barcodes_column = 1, has_headers = 
         option_menu_text_color = '#f7eacd'
 
     layout = [
-        [
-            sg.Text(translator('Choose Samples column:'),size=(25,1)),
-            sg.OptionMenu(column_headers, default_value=column_headers[int(samples_column)], 
-                          text_color=option_menu_text_color, key='-SAMPLES COLUMN-'),
-        ],
-        [
-            sg.Text(translator('Choose Barcodes column:'),size=(25,1)),
-            sg.OptionMenu(column_headers, default_value=column_headers[int(barcodes_column)], 
-                          text_color=option_menu_text_color, key='-BARCODES COLUMN-'),
-        ],
-        [
-            AltButton(button_text=translator('Save'),key='-SAVE-'),
-        ],
+        [sg.Sizer(500,0)],
+        # [
+        #     AltButton(button_text=translator('Save'),key='-SAVE-'),
+        # ],
         [
             sg.Table(values=samples_list, headings=column_headers, visible_column_map=visible_column_map,
-                     key='-TABLE-',expand_x=True,expand_y=True,num_rows=25,vertical_scroll_only=False),
+                     justification='left',
+                     display_row_numbers=True,auto_size_columns=True,alternating_row_color=theme['INPUT'],
+                    expand_x=True,expand_y=True,vertical_scroll_only=False, num_rows=min(12,len(samples_list)),
+                    key='-TABLE-')
         ],
-        #[
-        #sg.Button(button_text='Save',key='-SAVE-'),
-        #],
+        [sg.Sizer(0,8)],
+        [ 
+            sg.Push(),
+            sg.Column([[
+                sg.Text(translator('Choose Barcodes column:')),
+            ],[
+                sg.Text(translator('Choose Samples column:')),
+            ]], element_justification='right', pad=(8,0)),
+            sg.Column([[
+                sg.OptionMenu(column_headers, default_value=column_headers[int(barcodes_column)], 
+                            text_color=option_menu_text_color, key='-BARCODES COLUMN-'),
+            ],
+            [
+                sg.OptionMenu(column_headers, default_value=column_headers[int(samples_column)], 
+                            text_color=option_menu_text_color, key='-SAMPLES COLUMN-'),
+            ]], pad=(8,0)),
+            sg.Push()
+        ],
     ]
     panel = sg.Frame("", layout, border_width=0, relief="solid", pad=(0,16), expand_x=True, expand_y=True)
 
@@ -117,6 +128,7 @@ def create_parse_window(samples, window = None, samples_column = 0, barcodes_col
 
     new_window = sg.Window(title, layout, resizable=True, finalize=True,
                            font=consts.DEFAULT_FONT, icon=consts.ICON, margins=(0,0), element_padding=(0,0))
+    new_window.set_min_size((320,512))
 
     if window != None:
         window.close()
@@ -153,16 +165,13 @@ def view_samples(run_info, values, samples_key):
 
     return run_info
 
-def run_parse_window(window, samples, column_headers, font = None):
+def run_parse_window(window, samples, column_headers):
     while True:
         event, values = window.read()
         if event != None:
             log_event(f'{event} [parse columns window]')
 
         if event == 'Exit' or event == sg.WIN_CLOSED or event == '-CLOSE-':
-            break
-        elif event == '-SAVE-':
-
             try:
                 samples_column = column_headers.index(values['-SAMPLES COLUMN-'])
                 barcodes_column = column_headers.index(values['-BARCODES COLUMN-'])
@@ -170,19 +179,20 @@ def run_parse_window(window, samples, column_headers, font = None):
                 update_log(f'column {samples_column} selected for samples, column {barcodes_column} selected for barcodes')
 
                 if barcodes_column == samples_column:
-                    raise Exception('barcodes and samples must be 2 separate columns')
+                    raise Exception('same column for barcodes and samples', 'Select different columns for barcodes and samples IDs')
 
                 if check_for_duplicate_entries(samples, barcodes_column):
-                    raise Exception('specified barcodes column contains duplicates')
+                    raise Exception('duplicates in the barcodes column', 'Check the samples file for duplicate barcodes, or select a different column')
 
                 if check_for_duplicate_entries(samples, samples_column):
-                    raise Exception('specified samples column contains duplicates')
+                    raise Exception('duplicates in the samples column', 'Check the samples file for duplicate sample IDs, or select a different column')
 
 
                 window.close()
                 return samples_column, barcodes_column
             except Exception as err:
-                error_popup(err, font)
+                error_popup(err.args[0], err.args[1])
+            break
 
 
     window.close()
