307e51596c82e78e727265f31b6761552e901572
CorwinAnsley <coreyansley@gmail.com>
2023-08-02 18:11:45 +0100

    working on fixing translatiion issues

--- artifice/artifice_core/language.py
+++ artifice/artifice_core/language.py
@@ -50,10 +50,10 @@ def translate_text(string: str, language: str, scheme_list = None, append_scheme
     return return_string
 
 def setup_translator():
-    consts.retrieve_config()
+    config = consts.retrieve_config()
     translate_scheme = get_translate_scheme()
     try:
-        language = consts.config['LANGUAGE']
+        language = config['LANGUAGE']
     except:
         language = 'English'
         
