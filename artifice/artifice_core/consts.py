b415a54f2324dff2d23349d33590054b9f1f457b
GitHub Action <action@github.com>
2024-09-13 13:11:12 +0000

    incrementing version number

--- artifice/artifice_core/consts.py
+++ artifice/artifice_core/consts.py
@@ -187,7 +187,7 @@ PIRANHA_URL = 'https://github.com/polio-nanopore/piranha/'
 
 RAMPART_VERSION = None
 PIRANHA_VERSION = None
-PIRANHA_GUI_VERSION = '1.5.7'
+PIRANHA_GUI_VERSION = '1.6.0'
 
 config = None
 #LOGFILE = None
